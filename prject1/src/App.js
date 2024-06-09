637be8547bffbdae2a60894e2e0f14b4ac01cb25
vijaypal <vpsaini777@gmai.com>
2024-06-09 12:38:34 +0530

    Add new project

---
+++ prject1/src/App.js
@@ -0,0 +1,13 @@
+
+import Vj from "./Vj";
+function App() {
+  return (
+  <>
+    <h1>Hello World</h1>
+    <Vj/>
+
+    </>
+  );
+}
+
+export default App;
---
+++ prject1/src/Vj.js
@@ -0,0 +1,8 @@
+function Vj(){
+    return (
+        <h1>vijay this react project</h1>
+    )
+
+}
+
+export default Vj;
