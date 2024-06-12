c9f6dc349ecf8b0457f4ca5ddfc258b04c415607
vijaypal <vpsaini777@gmai.com>
2024-06-12 12:54:02 +0530

    New ReduxToolkit Project Upload

---
+++ reduxtoolkit-todo/src/app/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/TodoSlice';
+
+
+export const store = configureStore({
+    reducer: todoReducer
+})
+
+
+
