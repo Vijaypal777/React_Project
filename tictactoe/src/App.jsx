4c18881dc8ff83c314a7edd0df07b18b50c7ad9d
vijaypal <vpsaini777@gmai.com>
2024-07-06 12:57:10 +0530

    Tic Tac Toe Game Upload

---
+++ tictactoe/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Board from './tictactoeGame/Board'
+
+function App() {
+
+  return (
+    <div className='game-box'>
+      <Board/>
+    </div>
+  )
+}
+
+export default App
